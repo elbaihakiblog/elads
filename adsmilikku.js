9a160c37921ce4dcb31bbecab46c700e526099ba
elbaihakiblog <49315250+elbaihakiblog@users.noreply.github.com>
2019-04-05 22:06:49 +0700

    Add files via upload

---
+++ adsmilikku.js
@@ -0,0 +1,10 @@
+<script>
+//<![CDATA[
+var element = document.getElementById("insertad");
+element.innerHTML = "<ins class="adsbygoogle" style="display:inline-block;width:336px;height:280px" data-ad-client="ca-pub-7693031522315252" data-ad-slot="8509388929"></ins>";
+var newScript = document.createElement("script");
+var inlineScript = document.createTextNode("(adsbygoogle = window.adsbygoogle || []).push({});");
+newScript.appendChild(inlineScript);
+element.appendChild(newScript);
+//]]>
+</script>
